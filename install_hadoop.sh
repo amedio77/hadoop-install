030ea9b14f2e853b612ebc0a2f43489b451ad60e
이우송 <32665532+amedio77@users.noreply.github.com>
2020-03-17 15:49:38 +0900

    Update install_hadoop.sh

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -12,14 +12,14 @@ EOF
 
 chmod 600 ~/.ssh/kepri-msa.pem
 
-sudo -- sh -c "echo 10.0.0.5 hmaster >> /etc/hosts"
-sudo -- sh -c "echo 10.0.0.3 hworker-1 >> /etc/hosts"
-sudo -- sh -c "echo 10.0.0.15 hworker-2 >> /etc/hosts"
+#sudo -- sh -c "echo 10.0.0.5 hmaster >> /etc/hosts"
+#sudo -- sh -c "echo 10.0.0.3 hworker-1 >> /etc/hosts"
+#sudo -- sh -c "echo 10.0.0.15 hworker-2 >> /etc/hosts"
 
 
-sudo -- sh -c "echo hmaster >> /etc/ansible/hosts"
-sudo -- sh -c "echo hworker-1 >> /etc/ansible/hosts"
-sudo -- sh -c "echo hworker-2 >> /etc/ansible/hosts"
+#sudo -- sh -c "echo hmaster >> /etc/ansible/hosts"
+#sudo -- sh -c "echo hworker-1 >> /etc/ansible/hosts"
+#sudo -- sh -c "echo hworker-2 >> /etc/ansible/hosts"
 
 ansible-playbook  --private-key=~/.ssh/kepri-msa.pem add_known_hosts.yml
 
