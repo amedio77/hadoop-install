0b303f4894270c8dde8965c423bfe44f0f4bc12e
이우송 <32665532+amedio77@users.noreply.github.com>
2020-05-15 13:39:43 +0900

    Update install-host.sh

--- install-host.sh
+++ install-host.sh
@@ -1,4 +1,7 @@
 
-ansible-playbook --private-key ~/.ssh/kepri-msa.pem host-local-env.yml
+ansible-playbook --private-key ~/.ssh/kepri-msa.pem host-local-env.yml \
+ -e 'input_host="10.1.8.14 hmaster\n10.1.8.3 hworker-1\n10.1.8.5 hworker-2" \
+     input_ansible_host="[master]\nhmaster\n[workers]\nhworker-1\nhworker-2"'
 
-ansible-playbook --private-key ~/.ssh/kepri-msa.pem host-env.yml
+ansible-playbook --private-key ~/.ssh/kepri-msa.pem host-env.yml \
+ -e 'input_host="10.1.8.14 hmaster\n10.1.8.3 hworker-1\n10.1.8.5 hworker-2"'
