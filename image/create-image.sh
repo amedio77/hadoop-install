4019882d26df970bc276966ae054b5b438e95da4
이우송 <32665532+amedio77@users.noreply.github.com>
2020-06-25 18:29:59 +0900

    Update create-image.sh

--- image/create-image.sh
+++ image/create-image.sh
@@ -4,7 +4,7 @@ sudo yum install git -y
 # install wget
 sudo yum install wget -y
 
-# 하둡설치파일 세팅
+# 하둡설치파일 다운로드 세팅
 wget http://apache.claz.org/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz
 
 tar -xvzf hadoop-3.2.1.tar.gz
@@ -21,8 +21,7 @@ cp ~/hadoop-install/hadoop-env/*.*  ~/hadoop/etc/hadoop/
 sudo yum install -y java-1.8.0-openjdk-devel.x86_64
 
 # 환경변수 설정
-vi /home/centos/.bashrc
-
+cat <<EOF>>/home/centos/.bashrc
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.252.b09-2.el7_8.x86_64
 export HADOOP_HOME=~/hadoop/
 export HADOOP_INSTALL=$HADOOP_HOME
@@ -33,3 +32,15 @@ export HADOOP_YARN_HOME=$HADOOP_HOME
 export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
 export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$HADOOP_HOME/share/hadoop/tools/lib/*
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+EOF
+
+
+# dfs dir 생성
+mkdir -p ~/hadoop/pids
+mkdir -p ~/hadoop/etc/hadoop/dfs/data
+mkdir -p ~/hadoop/etc/hadoop/dfs/name
+
+chmod 7055 ~/hadoop/pids
+chmod 7055 ~/hadoop/etc/hadoop/dfs/data
+chmod 7055 ~/hadoop/etc/hadoop/dfs/name
+
